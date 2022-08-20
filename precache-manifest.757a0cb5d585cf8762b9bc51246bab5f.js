75a29cea4f850bf341f809b33d679ea51a08197c
Subramaniame-Veedhi <106572829+Subramaniame-Veedhi@users.noreply.github.com>
2022-08-20 22:12:39 +0530

    Updates

---
+++ precache-manifest.757a0cb5d585cf8762b9bc51246bab5f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1f012d8623c7eab9c799d2c4ffb895",
+    "url": "/movie-watch-list/index.html"
+  },
+  {
+    "revision": "c4df5129e27f747582a0",
+    "url": "/movie-watch-list/static/css/main.da937be7.chunk.css"
+  },
+  {
+    "revision": "c449d3de62daca75b166",
+    "url": "/movie-watch-list/static/js/2.e44de978.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/movie-watch-list/static/js/2.e44de978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4df5129e27f747582a0",
+    "url": "/movie-watch-list/static/js/main.7a78e460.chunk.js"
+  },
+  {
+    "revision": "3b96fa9b71d0d6ac484d",
+    "url": "/movie-watch-list/static/js/runtime-main.1f66353e.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/movie-watch-list/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/movie-watch-list/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/movie-watch-list/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/movie-watch-list/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/movie-watch-list/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/movie-watch-list/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/movie-watch-list/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/movie-watch-list/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/movie-watch-list/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/movie-watch-list/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/movie-watch-list/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/movie-watch-list/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/movie-watch-list/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/movie-watch-list/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/movie-watch-list/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
